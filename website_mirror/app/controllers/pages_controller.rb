7f0ce3e20d6a7286b84e03030217331f4af9c8d7
Bryan Taylor <bryan@bryan-taylor.com>
2011-10-05 01:06:10 -0400

    Done with pages

--- website_mirror/app/controllers/pages_controller.rb
+++ website_mirror/app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def about
+    @title = "About"
   end
 
   def blog
+    @title = "Blog"
   end
 
   def portfolio
+    @title = "Portfolio"
   end
 
   def contact
+    @title = "Contact"
   end
 end
 
--- website_mirror/spec/controllers/pages_controller_spec.rb
+++ website_mirror/spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "About :: Bryan Taylor")
     end
   end
 
@@ -14,12 +20,22 @@ describe PagesController do
       get 'blog'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'blog'
+      response.should have_selector("title", :content => "Blog :: Bryan Taylor")
+    end
   end
 
   describe "GET 'portfolio'" do
     it "should be successful" do
       get 'portfolio'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'portfolio'
+      response.should have_selector("title", :content => "Portfolio :: Bryan Taylor")
     end
   end
 
@@ -28,6 +44,12 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Contact :: Bryan Taylor")
+    end
   end
 
 end
+
