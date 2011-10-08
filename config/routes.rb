87de57a4dc765a5c0824093523262ad9709db076
Bryan Taylor <bryan@bryan-taylor.com>
2011-10-08 02:14:24 -0400

    Added scaffolding for Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WebsiteMirror::Application.routes.draw do
+  resources :posts
+
   match '/about',     :to => 'pages#about'
   match '/blog',      :to => 'pages#blog'
   match '/portfolio', :to => 'pages#portfolio'
