3afb8e7d912b4d7edbcf5a6786e70a6c5b778585
Bryan Taylor <bryan@bryan-taylor.com>
2011-10-09 01:02:42 -0400

    Revert "Introduced a simple blog interface.  Will finish up in the morning."
    This reverts commit 7f4b1cf3c9c03253def5e0492dbfc30fd267f6a2.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,2 @@
 class Post < ActiveRecord::Base
-  default_scope :order => 'posts.created_at DESC'
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ WebsiteMirror::Application.routes.draw do
   resources :posts
 
   match '/about',     :to => 'pages#about'
-  match '/blog',      :to => 'Posts#index', :as => "blog"
+  match '/blog',      :to => 'pages#blog'
   match '/portfolio', :to => 'pages#portfolio'
   match '/contact',   :to => 'pages#contact'
 
